ed78522afe74b48249f072650f3d295de48a6482
yao0510 <sf163864@gmail.com>
2018-12-14 01:19:54 +0800

    best up to 1.58

--- hw4/0516075_hw4.cpp
+++ hw4/0516075_hw4.cpp
@@ -15,9 +15,7 @@ using namespace std;
 #define MYBLUE	0
 
 int imgWidth, imgHeight;
-int MEAN_FILTER_SIZE=9;
 int SOBEL_FILTER_SIZE;
-int FILTER_SCALE;
 int *filter_gx, *filter_gy;
 
 sem_t sem[1861][2631];
@@ -38,7 +36,9 @@ const char *outputMed_name[5] = {
 };
 
 
-unsigned char *pic_in, *pic_grey, *pic_mean, *pic_gx, *pic_gy, *pic_sobel,*pic_final;
+unsigned char *pic_in,*pic_final;
+
+unsigned char pic_grey[1861*2631],pic_mean[1861*2631],pic_gx[1861*2631],pic_gy[1861*2631],pic_sobel[1861*2631];
 
 void *compute_mean(void* idx)
 {
@@ -47,18 +47,27 @@ void *compute_mean(void* idx)
 
 			int tmp = 0;
 			int a, b ,k=0,sum=0;
-			int ws=3;
-			for (int j = 0; j<ws; j++)
-			for (int i = 0; i<ws; i++)
-			{
-				a = w + i - (ws / 2);
-				b = h + j - (ws / 2);
+			for (int j = 0; j<3; j++){
+				a = w + 0 - 1;
+				b = h + j - 1;
 				// detect for borders of the image
-				if (a<0 || b<0 || a>=imgWidth || b>=imgHeight)continue;
-				sum=sum+pic_grey[b*imgWidth + a];
-			};
+				if (a>=0 && b>=0 && a<imgWidth && b<imgHeight)
+					sum=sum+pic_grey[b*imgWidth + a];
 
-			tmp=sum/MEAN_FILTER_SIZE;
+				a = w + 1 - 1;
+				b = h + j - 1;
+				// detect for borders of the image
+				if (a>=0 && b>=0 && a<imgWidth && b<imgHeight)
+					sum=sum+pic_grey[b*imgWidth + a];
+
+				a = w + 2 - 1;
+				b = h + j - 1;
+				// detect for borders of the image
+				if (a>=0 && b>=0 && a<imgWidth && b<imgHeight)
+					sum=sum+pic_grey[b*imgWidth + a];
+			}
+
+			tmp=sum/9;
 			
 			if (tmp < 0) tmp = 0;
 			if (tmp > 255) tmp = 255;
@@ -71,62 +80,55 @@ void *compute_mean(void* idx)
 	pthread_exit(NULL);
 }
 
-unsigned char sobelFilter(int w, int h)
+void * compute_sobel(void* idx)
 {
-	int tmp = 0,tmp2=0;
-	int a, b;
-	// int ws = (int)sqrt((float)SOBEL_FILTER_SIZE);
-	int ws=3;
-	for (int j = 0; j<ws; j++)
-	for (int i = 0; i<ws; i++)
-	{
-		a = w + i - (ws / 2);
-		b = h + j - (ws / 2);
+	for (int h = 0; h < imgHeight; h++){
+        for (int w = 0; w < imgWidth; w++){
+            if(h == imgHeight-1 && w == imgWidth-1)sem_wait(&sem[h][w]);
+            else if(w == imgWidth-1)sem_wait(&sem[h+1][w]);
+            else if(h == imgHeight-1)sem_wait(&sem[h][w+1]);
+            else sem_wait(&sem[h+1][w+1]);
+			
+			int tmp = 0,tmp2=0;
+			int a, b;
+			for (int j = 0; j<3; j++)
+			for (int i = 0; i<3; i++)
+			{
+				a = w + i - 1;
+				b = h + j - 1;
 
-		// detect for borders of the image
-		if (a<0 || b<0 || a>=imgWidth || b>=imgHeight) continue;
+				// detect for borders of the image
+				if (a<0 || b<0 || a>=imgWidth || b>=imgHeight) continue;
 
-		tmp += filter_gx[j*ws + i] * pic_mean[b*imgWidth + a];
-		tmp2 += filter_gy[j*ws + i] * pic_mean[b*imgWidth + a];
-	};
-	if (tmp < 0) tmp = 0;
-	else if (tmp > 255) tmp = 255;
-	if (tmp2 < 0) tmp2 = 0;
-	else if (tmp2 > 255) tmp2 = 255;
+				tmp += filter_gx[j*3 + i] * pic_mean[b*imgWidth + a];
+				tmp2 += filter_gy[j*3 + i] * pic_mean[b*imgWidth + a];
+			};
+			if (tmp < 0) tmp = 0;
+			else if (tmp > 255) tmp = 255;
+			if (tmp2 < 0) tmp2 = 0;
+			else if (tmp2 > 255) tmp2 = 255;
 
-	pic_gx[h*imgWidth + w]=tmp;
-	pic_gy[h*imgWidth + w]=tmp2;
+			pic_gx[h*imgWidth + w]=tmp;
+			pic_gy[h*imgWidth + w]=tmp2;
 
-	tmp = 0;
-	tmp = sqrt(pic_gx[h*imgWidth + w]*pic_gx[h*imgWidth + w] + pic_gy[h*imgWidth + w]*pic_gy[h*imgWidth + w]);
-	if (tmp < 0) tmp = 0;
-	if (tmp > 255) tmp = 255;
+			tmp = 0;
+			tmp = sqrt(pic_gx[h*imgWidth + w]*pic_gx[h*imgWidth + w] + pic_gy[h*imgWidth + w]*pic_gy[h*imgWidth + w]);
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
 
-	pic_sobel[h*imgWidth + w]=tmp;
-}
+			pic_sobel[h*imgWidth + w]=tmp;
 
-void *compute_sobel(void* idx){
-
-    //apply the gy_sobel filter to the image 
-    for (int j = 0; j < imgHeight; j++){
-        for (int i = 0; i < imgWidth; i++){
-            if(j == imgHeight-1 && i == imgWidth-1)sem_wait(&sem[j][i]);
-            else if(i == imgWidth-1)sem_wait(&sem[j+1][i]);
-            else if(j == imgHeight-1)sem_wait(&sem[j][i+1]);
-            else sem_wait(&sem[j+1][i+1]);
-			sobelFilter(i, j);
-			pic_final[3 * (j*imgWidth + i) + MYRED] = pic_sobel[j*imgWidth + i];
-			pic_final[3 * (j*imgWidth + i) + MYGREEN] = pic_sobel[j*imgWidth + i];
-			pic_final[3 * (j*imgWidth + i) + MYBLUE] = pic_sobel[j*imgWidth + i];
-			if(j == imgHeight-1 && i == imgWidth-1)sem_post(&sem[j][i]);
-            else if(i == imgWidth-1)sem_post(&sem[j+1][i]);
-            else if(j == imgHeight-1)sem_post(&sem[j][i+1]);
-            else sem_post(&sem[j+1][i+1]);
+			pic_final[3 * (h*imgWidth + w) + MYRED] = pic_sobel[h*imgWidth + w];
+			pic_final[3 * (h*imgWidth + w) + MYGREEN] = pic_sobel[h*imgWidth + w];
+			pic_final[3 * (h*imgWidth + w) + MYBLUE] = pic_sobel[h*imgWidth + w];
+			if(h == imgHeight-1 && w == imgWidth-1)sem_post(&sem[h][w]);
+            else if(w == imgWidth-1)sem_post(&sem[h+1][w]);
+            else if(h == imgHeight-1)sem_post(&sem[h][w+1]);
+            else sem_post(&sem[h+1][w+1]);
         }
     }
-    pthread_exit(NULL);
+	pthread_exit(NULL);
 }
-
 
 int main()
 {
@@ -149,50 +151,152 @@ int main()
 		fscanf(mask, "%d", &filter_gy[i]);
 
 	fclose(mask);
-	
 	BmpReader* bmpReader = new BmpReader();
-	for (int k = 0; k<5; k++){
 
-		// read input BMP file
-		pic_in = bmpReader->ReadBMP(inputfile_name[k], &imgWidth, &imgHeight);
-		// allocate space for output image
-		pic_grey = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_mean = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_gx = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_gy = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_sobel = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+	// read input BMP file
+	pic_in = bmpReader->ReadBMP(inputfile_name[0], &imgWidth, &imgHeight);
+	// allocate space for output image
+	pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
 
-		for (int h = 0; h<imgHeight; h++) {
-			for (int w = 0; w<imgWidth; w++){
-				sem_init(&sem[h][w],0,0);
-				int tmp = (
-				pic_in[3 * (h*imgWidth + w) + MYRED] +
-				pic_in[3 * (h*imgWidth + w) + MYGREEN] +
-				pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
-				if (tmp < 0) tmp = 0;
-				if (tmp > 255) tmp = 255;
-				pic_grey[h*imgWidth + w] = tmp;
-			}
+	for (int h = 0; h<imgHeight; h++) {
+		for (int w = 0; w<imgWidth; w++){
+			sem_init(&sem[h][w],0,0);
+			int tmp = (
+			pic_in[3 * (h*imgWidth + w) + MYRED] +
+			pic_in[3 * (h*imgWidth + w) + MYGREEN] +
+			pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
+			pic_grey[h*imgWidth + w] = tmp;
 		}
-
-        pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
-        pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
-		
-        pthread_join(t1,NULL);
-        pthread_join(t2,NULL);
-
-		bmpReader->WriteBMP(outputMed_name[k], imgWidth, imgHeight, pic_final);
-
-		//free memory space
-		free(pic_in);
-		free(pic_grey);
-		free(pic_mean);
-		free(pic_final);
-		free(pic_sobel);
-		free(pic_gx);
-		free(pic_gy);
 	}
+
+	pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
+	pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+
+	bmpReader->WriteBMP(outputMed_name[0], imgWidth, imgHeight, pic_final);
+
+	//free memory space
+	free(pic_in);
+	free(pic_final);
+
+	// read input BMP file
+	pic_in = bmpReader->ReadBMP(inputfile_name[1], &imgWidth, &imgHeight);
+	// allocate space for output image
+	pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+
+	for (int h = 0; h<imgHeight; h++) {
+		for (int w = 0; w<imgWidth; w++){
+			sem_init(&sem[h][w],0,0);
+			int tmp = (
+			pic_in[3 * (h*imgWidth + w) + MYRED] +
+			pic_in[3 * (h*imgWidth + w) + MYGREEN] +
+			pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
+			pic_grey[h*imgWidth + w] = tmp;
+		}
+	}
+
+	pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
+	pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+
+	bmpReader->WriteBMP(outputMed_name[1], imgWidth, imgHeight, pic_final);
+
+	//free memory space
+	free(pic_in);
+	free(pic_final);
+
+	// read input BMP file
+	pic_in = bmpReader->ReadBMP(inputfile_name[2], &imgWidth, &imgHeight);
+	// allocate space for output image
+	pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+
+	for (int h = 0; h<imgHeight; h++) {
+		for (int w = 0; w<imgWidth; w++){
+			sem_init(&sem[h][w],0,0);
+			int tmp = (
+			pic_in[3 * (h*imgWidth + w) + MYRED] +
+			pic_in[3 * (h*imgWidth + w) + MYGREEN] +
+			pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
+			pic_grey[h*imgWidth + w] = tmp;
+		}
+	}
+
+	pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
+	pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+
+	bmpReader->WriteBMP(outputMed_name[2], imgWidth, imgHeight, pic_final);
+
+	//free memory space
+	free(pic_in);
+	free(pic_final);
+
+	// read input BMP file
+	pic_in = bmpReader->ReadBMP(inputfile_name[3], &imgWidth, &imgHeight);
+	// allocate space for output image
+	pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+
+	for (int h = 0; h<imgHeight; h++) {
+		for (int w = 0; w<imgWidth; w++){
+			sem_init(&sem[h][w],0,0);
+			int tmp = (
+			pic_in[3 * (h*imgWidth + w) + MYRED] +
+			pic_in[3 * (h*imgWidth + w) + MYGREEN] +
+			pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
+			pic_grey[h*imgWidth + w] = tmp;
+		}
+	}
+
+	pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
+	pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+
+	bmpReader->WriteBMP(outputMed_name[3], imgWidth, imgHeight, pic_final);
+
+	//free memory space
+	free(pic_in);
+	free(pic_final);
+
+	// read input BMP file
+	pic_in = bmpReader->ReadBMP(inputfile_name[4], &imgWidth, &imgHeight);
+	// allocate space for output image
+	pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+
+	for (int h = 0; h<imgHeight; h++) {
+		for (int w = 0; w<imgWidth; w++){
+			sem_init(&sem[h][w],0,0);
+			int tmp = (
+			pic_in[3 * (h*imgWidth + w) + MYRED] +
+			pic_in[3 * (h*imgWidth + w) + MYGREEN] +
+			pic_in[3 * (h*imgWidth + w) + MYBLUE] )/3;
+			if (tmp < 0) tmp = 0;
+			if (tmp > 255) tmp = 255;
+			pic_grey[h*imgWidth + w] = tmp;
+		}
+	}
+
+	pthread_create(&t1,NULL,compute_mean,(void*) &id[0]);
+	pthread_create(&t2,NULL,compute_sobel,(void*) &id[0]);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+
+	bmpReader->WriteBMP(outputMed_name[4], imgWidth, imgHeight, pic_final);
+
+	//free memory space
+	free(pic_in);
+	free(pic_final);
 
 	return 0;
 }
